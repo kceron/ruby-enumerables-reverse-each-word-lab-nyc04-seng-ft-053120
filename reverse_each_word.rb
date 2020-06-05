6cbad9f992d23bc7b9364fc4f221bfdfea8781b7
Karem Ceron <karemiceron@gmail.com>
2020-06-05 15:11:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,4 +2,10 @@ def reverse_each_word(string)
   array = string.split 
   reversed = array.collect {|elem| elem.reverse}
   reversed.join(" ")
+=end
+
+def reverse_each_word(string)
+  array = string.split 
+  reversed = array.each {|elem| elem.reverse}
+  reversed.join(" ")
 end
