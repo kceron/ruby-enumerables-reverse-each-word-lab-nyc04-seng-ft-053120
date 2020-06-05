67a764c74380367a339d75236e2e9d50fea0930f
Karem Ceron <karemiceron@gmail.com>
2020-06-05 15:24:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,6 @@
-=begin 
+begin 
 def reverse_each_word(string)
   array = string.split 
   reversed = array.collect {|elem| elem.reverse}
   reversed.join(" ")
-=end
-
-def reverse_each_word(string)
-  array = string.split 
-  reversed  = []
-  array.each do 
-    final_string = ""
-    |elem| elem.reverse
-    final_string << elem
-  end 
-  final_string
 end
